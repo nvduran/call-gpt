ef3830ab1934db9bf3c5de35f54a9561d283c642
nvduran <nvduran@gmail.com>
2024-07-29 14:38:09 -0500

    first call made with error

--- scripts/outbound-call.js
+++ scripts/outbound-call.js
@@ -3,21 +3,26 @@
   to your own mobile phone.
 */
 
-require('dotenv').config();
+require("dotenv").config();
 
 async function makeOutBoundCall() {
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  
-  const client = require('twilio')(accountSid, authToken);
+        const accountSid = process.env.TWILIO_ACCOUNT_SID;
+        const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-  await client.calls
-    .create({
-      url: `https://${process.env.SERVER}/incoming`,
-      to: process.env.YOUR_NUMBER,
-      from: process.env.FROM_NUMBER
-    })
-    .then(call => console.log(call.sid));
+        if (!accountSid || !authToken) {
+                console.error("TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN must be set in the environment variables.");
+                return;
+        }
+
+        const client = require("twilio")(accountSid, authToken);
+
+        await client.calls
+                .create({
+                        url: `https://${process.env.SERVER}/incoming`,
+                        to: process.env.YOUR_NUMBER,
+                        from: process.env.FROM_NUMBER,
+                })
+                .then((call) => console.log(call.sid));
 }
 
 makeOutBoundCall();
